497e1247581b874148dde2de5eaa7a1839fe2d3a
Svetoslav Pachev <svetoslavpachevvv@gmail.com>
2022-03-30 11:42:05 +0200

    searchfield and cardlist connected with state

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
-import React from "react";
+import React, { Component } from "react";
 import CardList from "./CardList";
 import { robots } from "./robots";
 import SearchBox from "./Searchbox";
+import "./App.css"
 
 
-const App = () => {
-  return (
-    <div className="tc">
-      <h1>Robots</h1>
-      <SearchBox />
-      <CardList robots={robots} />
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      robots: robots,
+      searchField: ''
+    }
+  }
+
+  onSearchChange = (event) => {
+    this.setState({ searchField: event.target.value })
+  }
+
+
+  render() {
+    const filteredRobots = this.state.robots.filter(robots => {
+    return robots.name.toLowerCase().includes(this.state.searchField.toLowerCase());
+    })
+    return (
+      <div className="tc">
+        <h1 className="f1">Robots</h1>
+        <SearchBox searchChange={this.onSearchChange}/>
+        <CardList robots={filteredRobots} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App'
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
-import { robots } from './robots'
 
 ReactDOM.render(
     <App />,
