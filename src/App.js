be78772af4aef0b749588dc71d682396c9d4a0fd
Svetoslav Pachev <svetoslavpachevvv@gmail.com>
2022-03-30 15:49:01 +0200

    scrollble cardlist

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import CardList from "./CardList";
-import { robots } from "./robots";
 import SearchBox from "./Searchbox";
+import Scroll from "./Scroll"
 import "./App.css"
 
 
@@ -9,9 +9,15 @@ class App extends Component {
   constructor() {
     super()
     this.state = {
-      robots: robots,
+      robots: [],
       searchField: ''
     }
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(users => this.setState({ robots: users }));
   }
 
   onSearchChange = (event) => {
@@ -23,13 +29,19 @@ class App extends Component {
     const filteredRobots = this.state.robots.filter(robots => {
     return robots.name.toLowerCase().includes(this.state.searchField.toLowerCase());
     })
-    return (
-      <div className="tc">
-        <h1 className="f1">Robots</h1>
-        <SearchBox searchChange={this.onSearchChange}/>
-        <CardList robots={filteredRobots} />
-      </div>
-    );
+    if (this.state.robots.length === 0 ) {
+      return <h1>Loading</h1>
+    } else {
+      return (
+        <div className="tc">
+          <h1 className="f1">Robots</h1>
+          <SearchBox searchChange={this.onSearchChange}/>
+          <Scroll>
+            <CardList robots={filteredRobots} />
+          </Scroll>
+        </div>
+      );
+    }
   }
 }
 
